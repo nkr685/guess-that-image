8695e6c3f46d28d0c1e08b1d26af6f0c51ccf7c2
Ben McCabe <benmccabe19@gmail.com>
2024-03-06 18:13:32 -0500

    Game shows answer for testing purposes

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,15 +3,15 @@ import { GameContext } from "../context/GameContext";
 
 const ScoreBoard = () => {
     // global vars
-    const { gameActive, score, correct, incorrect} = useContext(GameContext)
+    const { gameActive, score, correct, incorrect, imageName } = useContext(GameContext)
 
     // local vars
-    const [ scoreResults, setScoreResults] = useState(`Score: ${score} | Correct: ${correct} | Incorrect: ${incorrect}`)
+    const [ scoreResults, setScoreResults] = useState(`Score: ${score} | Correct: ${correct} | Incorrect: ${incorrect} | Answer: ${imageName}`)
 
     // sets score display every time anytime score, correct, mistakes updates
     useEffect(() => {
         if (gameActive) {
-            setScoreResults(`Score: ${score} | Correct: ${correct} | Incorrect: ${incorrect}`)
+            setScoreResults(`Score: ${score} | Correct: ${correct} | Incorrect: ${incorrect} | Answer: ${imageName}`)
         } else {
             setScoreResults(`Final Score: ${score} | Correct: ${correct} | Incorrect: ${incorrect}`)
         }
